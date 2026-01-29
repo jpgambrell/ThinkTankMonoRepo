cbfe33da57b696db9c169f7a8d0de715c4469e63
jpgambrell <john.gambrell@gmail.com>
2026-01-29 10:05:46 -0600

    Initial Commit

---
+++ ThinkTank/ThinkTankApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkTankApp.swift
+//  ThinkTank
+//
+//  Created by John Gambrell on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ThinkTankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
